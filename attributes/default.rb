95cc0f6aa8013292b388c39cdee50cd92709125c
Luke Randall <luke.randall@gmail.com>
2012-09-04 15:56:17 +0200

    Default UseDNS to false

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,5 @@ default[:sshd][:port] = 22
 default[:sshd][:protocol] = 2
 default[:sshd][:password_auth] = false
 default[:sshd][:root_login] = false
+default[:sshd][:use_dns] = false
 default[:sshd][:config_dir] = "/etc/ssh"
